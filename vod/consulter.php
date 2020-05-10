2fd837d76462875e350a21277f648d0eab26e4e8
Arthur <arthur.cathelineau.sio@gmail.com>
2020-05-10 19:55:36 +0200

    Version 1

---
+++ vod/consulter.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>VOD-Consulter</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="style/style.css" />
+    </head>
+    
+    
+	<body>
+	<table>
+		<tr id="tetetab"><td>Film</td><td>Année</td><td>Producteur</td></tr>
+	<?php
+	$lines = file("data/vod.csv");
+	foreach($lines as $n => $line){
+		$elements = explode(":",$line);
+		echo "<tr><td>$elements[0]</td><td>$elements[1]</td><td>$elements[2]</td></tr>";
+	}
+	?>
+	</table>
+	</body>
---
+++ vod/recherche.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>VOD-Recherche de films </title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="style/style.css" />
+    </head>
+    <body>
+
+		
+		<?php
+		$lines = file("data/vod.csv");
+		
+		$resultat = false ;
+		
+		foreach($lines as $n => $line){
+		$unfilm = explode(":", $line);
+		
+		
+		if (strtoupper($unfilm[0]) == strtoupper($_GET["nom"])) {
+			$resultat = true ;
+			echo "<center><p id='texte'>Voici le film que vous avez demandé :</p></center>";
+			echo "
+			<table>
+			<tr id='tetetab'><td>Film</td><td>Année</td><td>Producteur</td></tr>
+			<tr><td>$unfilm[0]</td><td>$unfilm[1]</td><td>$unfilm[2]</td></tr>
+			</table>
+			" ;
+			break;
+			}
+		}
+		
+		if ($resultat == false){
+			echo "<center><p id='texte'>Désolé, nous n'avons pas trouvé votre film.</p></center>" ;
+			}
+
+		
+
+		?>
+
+    </body>
